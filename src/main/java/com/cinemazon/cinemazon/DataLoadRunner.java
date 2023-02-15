43247b6c79d8942471de7120cc98de1474c1856d
EleonoraPisacane <eleonora.pisacane@yahoo.it>
2023-02-15 10:09:47 +0100

    aggiunti dati al dataloader

--- src/main/java/com/cinemazon/cinemazon/DataLoadRunner.java
+++ src/main/java/com/cinemazon/cinemazon/DataLoadRunner.java
@@ -30,17 +30,21 @@ public class DataLoadRunner implements CommandLineRunner {
         auditoriumService.createAuditorium(a1);
         auditoriumService.createAuditorium(a2);
 
-        Movie m1 = new Movie("Everything Everywhere all at once", "Daniels", 140, false, false, false, false, false, true, "https://pokeapi.co/api/v2/berry-firmness/2/");
-        Movie m2 = new Movie("Cena con delitto - Knives out", "Rian Johnson", 130, false, true, true, false, false, true, "https://pokeapi.co/api/v2/berry/64/");
-        Movie m3 = new Movie("Avatar - La via dell'acqua", "James Cameron", 192, true, true, true, false, false, true, "https://pokeapi.co/api/v2/berry/64/");
+        Movie m1 = new Movie("Everything Everywhere all at once", "Daniels", 140, false, false, false, false, false, true, "https://tinyurl.com/2k98tpkj");
+        Movie m2 = new Movie("Cena con delitto - Knives out", "Rian Johnson", 130, false, true, true, false, false, true, "https://tinyurl.com/46x92y32");
+        Movie m3 = new Movie("Avatar - La via dell'acqua", "James Cameron", 192, true, true, true, false, false, true, "https://tinyurl.com/mr3bv2m8");
+        Movie m4 = new Movie("Shining - Remastered", "Stanley Kubrick", 144, false, true, true, true, false, true, "https://tinyurl.com/yrvf3n7x");
         movieService.createMovie(m1);
         movieService.createMovie(m2);
         movieService.createMovie(m3);
+        movieService.createMovie(m4)
 
         showService.createShow(new MovieShow(LocalDate.of(2023, 03, 02), LocalTime.of(20,30), m1, a1));
         showService.createShow(new MovieShow(LocalDate.of(2023, 03, 02), LocalTime.of(20,00), m2, a2));
         showService.createShow(new MovieShow(LocalDate.of(2023, 03, 03), LocalTime.of(19,30), m3, a1));
         showService.createShow(new MovieShow(LocalDate.of(2023, 03, 03), LocalTime.of(21,30), m1, a2));
+        showService.createShow(new MovieShow(LocalDate.of(2023, 03, 03), LocalTime.of(23,30), m1, a1));
+
         System.out.println("Application started");
     }
 }
