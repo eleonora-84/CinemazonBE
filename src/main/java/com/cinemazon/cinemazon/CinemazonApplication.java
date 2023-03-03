369dae89562acb301136b58c018f63b9d7192ead
EleonoraPisacane <eleonora.pisacane@yahoo.it>
2023-03-03 17:02:02 +0100

    fix minori

--- src/main/java/com/cinemazon/cinemazon/CinemazonApplication.java
+++ src/main/java/com/cinemazon/cinemazon/CinemazonApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-// @Import(WebConfig.class) TODO
 public class CinemazonApplication {
 
 	public static void main(String[] args) {
